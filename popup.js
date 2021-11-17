62941edd32c5afba017232736c08ce55c69a4995
lawric1 <67882089+lawric1@users.noreply.github.com>
2021-11-17 12:02:23 -0300

    .

--- popup.js
+++ popup.js
@@ -1,28 +1,40 @@
 var inputField = document.getElementById("inputField");
 var addBtn = document.getElementById("addBtn");
 var taskList = document.getElementById("taskList");
-taskList.style.padding = '8px';
 
 function addTask() {
     var task = document.createElement('li');
-    task.style.cssText = `
-    display: flex; 
-    align-items: center;
-    justify-content: space-between;
-`
+    task.classList.add("task");
 
     var taskDescription = document.createElement('p');
+    task.classList.add("description");
     taskDescription.innerHTML = inputField.value;
-
+    
     var checkbox = document.createElement('input');
     checkbox.type = "checkbox";
-    // // checkbox.id = "id";
 
-    task.appendChild(taskDescription);
-    task.appendChild(checkbox);
-    taskList.appendChild(task);
-
+    if (taskDescription.innerHTML) {
+        task.appendChild(taskDescription);
+        task.appendChild(checkbox);
+        taskList.appendChild(task);    
+    }
+    
     inputField.value = ''
+
+    checkbox.addEventListener("click", () => {
+        if (checkbox.checked) {
+            taskDescription.style.textDecorationLine = "line-through";
+            taskDescription.style.opacity = "0.4";
+        } else {
+            taskDescription.style.textDecorationLine = "None";
+            taskDescription.style.opacity = "1";
+        }
+    }, false);
 }
+// inputField.addEventListener("keydown", (event) => {
+//     if (event.code === "Enter") {
+//         addTask();
+//     }
+// }, false);
 
 addBtn.addEventListener("click", addTask, false);
